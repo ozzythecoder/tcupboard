a79686d1cec6309bc70ebd1326dc6871f331b91c
ozzythecoder <august.chm@gmail.com>
2026-02-25 16:43:02 -0600

    add ssl cert to backend db connection logic

--- apps/backend/config/db.js
+++ apps/backend/config/db.js
@@ -1,25 +1,24 @@
 // db.js
-import pkg from 'pg';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import pkg from "pg";
 
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL
+    connectionString: process.env.DB_URL,
+    ssl: process.env.DB_SSL_CERT ?? false,
 });
 
 // Whenever a brand-new connection is created, set the search_path
-pool.on('connect', (client) => {
-  const schema = process.env.DB_SCHEMA || 'public';
-  client
-    .query(`SET search_path TO ${schema}, public`)
-    .then(() => {
-      console.log(`search_path set to ${schema}, public`);
-    })
-    .catch((err) => {
-      console.error('Error setting search_path:', err);
-    });
+pool.on("connect", (client) => {
+    const schema = process.env.DB_SCHEMA || "public";
+    client
+        .query(`SET search_path TO ${schema}, public`)
+        .then(() => {
+            console.log(`search_path set to ${schema}, public`);
+        })
+        .catch((err) => {
+            console.error("Error setting search_path:", err);
+        });
 });
 
 export default pool;
