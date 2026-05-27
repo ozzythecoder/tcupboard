c2bd0e958bd31d0f5954b9a18656bfb6633c799d
ozzythecoder <august.chm@gmail.com>
2026-05-27 14:07:41 -0500

    clarify intent of ProfileSync; purge console logs

--- apps/frontend/src/pages/User/Components/ProfileSync.js
+++ apps/frontend/src/pages/User/Components/ProfileSync.js
@@ -3,6 +3,21 @@ import { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 
+// what the hell does this file do?
+// - if user is authenticated:
+//      - sends POST request to /users/profile with user details
+//      - does nothing with the response
+// 
+// Seems like a hack to sync users from Auth0 db to Postgres db. Find alternative?
+// Auth0 doesn't allow much sync functionality on the free tier...
+// 
+// issues:
+// - network request and db query on every component load
+// - odd architecture
+// 
+// solution:
+// - running post-login/register hook in Auth0 (implemented)
+
 function ProfileSync() {
   const { isAuthenticated, getAccessTokenSilently, user } = useAuth0();
   const apiUrl = import.meta.env.VITE_API_URL;
@@ -16,31 +31,22 @@ function ProfileSync() {
       const saveUserProfile = async () => {
         try {
           const token = await getAccessTokenSilently();
-          console.log('Token retrieved:', !!token);
-          console.log('Complete user object being sent to backend:', user);
-
+          // console.log('Token retrieved:', !!token);
+          // console.log('Complete user object being sent to backend:', user);
           
-          // Use the correct API URL from your environment
-          // If apiUrl is "http://localhost:3001/api" then this becomes
-          // "http://localhost:3001/api/users/profile"
-          const endpoint = `${apiUrl}/users/profile`;
-          console.log('Making request to:', endpoint);
-          
-          const response = await axios.post(`${apiUrl}/users/profile`, {
+          const _response = await axios.post(`${apiUrl}/users/profile`, {
             sub: user.sub,
             email: user.email,
             name: user.name,
             nickname: user.nickname,
             // Make sure we include the custom claim specifically
-            [`https://tcupboard.org/username`]: user[`https://tcupboard.org/username`],
+            "https://tcupboard.org/username": user[`https://tcupboard.org/username`],
             preferred_username: user.preferred_username
           }, {
             headers: {
               Authorization: `Bearer ${token}`
             }
           });
-          
-          console.log('Profile saved successfully:', response.data);
         } catch (error) {
           console.error('Error in saveUserProfile:', error);
           if (error.response) {
