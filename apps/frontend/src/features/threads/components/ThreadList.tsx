c318a7976eb713c5015e33a846ffec4df74fe3f9
ozzythecoder <august.chm@gmail.com>
2026-06-02 15:08:05 -0500

    thread list - css stuff

--- apps/frontend/src/features/threads/components/ThreadList.tsx
+++ apps/frontend/src/features/threads/components/ThreadList.tsx
@@ -1,13 +1,13 @@
 import type { ForumMessageWithReplyDetails } from "#/types/resources";
 import { Avatar } from "@skeletonlabs/skeleton-react";
-import { utils } from "#/utils/formatDate";
+import { utils } from "#/utils/utils";
 import { Link } from "@tanstack/react-router";
 
 export function ThreadListItem({ thread }: { thread: ForumMessageWithReplyDetails }) {
     const createdAt = utils.formatDate(thread.created_at);
 
     return (
-        <li className="card p-4 grid grid-cols-[auto_1fr] preset-filled-surface-50-950 has-[.-thread-title-anchor:hover]:preset-filled-primary-50-950">
+        <li className="card p-4 grid grid-cols-[auto_1fr] preset-filled-surface-100-900 has-[.-thread-title-anchor:hover]:preset-filled-primary-50-950 overflow-hidden">
             <div className="flex flex-col items-center gap-4 w-25">
                 <Link to="/" from="/threads/">
                     <Avatar className="size-14 hover:brightness-110">
@@ -16,13 +16,17 @@ export function ThreadListItem({ thread }: { thread: ForumMessageWithReplyDetail
                     </Avatar>
                 </Link>
                 <Link to="/" from="/threads/">
-                    <p className="anchor text-sm text-surface-400-600">{thread.author}</p>
+                    <p className="anchor text-xs text-surface-400-600">{thread.author}</p>
                 </Link>
             </div>
-            <Link to="/threads/$id" params={{ id: thread.id!.toString() }} className="-thread-title-anchor group">
-                <div className="flex flex-col justify-between gap-4 h-full">
+            <Link
+                to="/threads/$id"
+                params={{ id: thread.id!.toString() }}
+                className="-thread-title-anchor group"
+            >
+                <div className="flex flex-col justify-between gap-4 h-full break-normal">
                     <h3 className="h6 group-hover:underline">{thread.title}</h3>
-                    <p className="text-sm text-surface-400-600">
+                    <p className="text-xs text-surface-400-600">
                         {createdAt} &bull; {thread.replyCount ?? 0}{" "}
                         {thread.replyCount === 1 ? "reply" : "replies"}
                     </p>
