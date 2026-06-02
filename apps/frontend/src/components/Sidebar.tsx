59eaea70e5c979ac90d663952ef6410439eecfb8
ozzythecoder <august.chm@gmail.com>
2026-06-02 15:01:40 -0500

    feat: proper collapsible sidebar

--- apps/frontend/src/components/Sidebar.tsx
+++ apps/frontend/src/components/Sidebar.tsx
@@ -1,11 +1,70 @@
+import { createContext, use, useContext, useEffect, useRef, useState } from "react";
+import type { Dispatch, SetStateAction } from "react";
 import { Navigation } from "./Nav";
+import { MenuIcon } from "lucide-react";
+
+interface ISidebarContext {
+    open: boolean;
+    setOpen: Dispatch<SetStateAction<boolean>>;
+    toggleOpen: () => void;
+}
+
+const SidebarContext = createContext<ISidebarContext>({} as ISidebarContext);
+
+export function SidebarContextProvider({ children }: { children: React.ReactNode }) {
+    const [open, setOpen] = useState(false);
+    const toggleOpen = () => {
+        setOpen((p) => !p);
+    };
+
+    return <SidebarContext value={{ open, setOpen, toggleOpen }}>{children}</SidebarContext>;
+}
+
+export const useSidebarContext = () => useContext(SidebarContext);
 
 export function Sidebar() {
-    
+    const { open, setOpen } = useSidebarContext();
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        function clickOutside(e: PointerEvent) {
+            if (open && ref.current && !ref.current.contains(e.target)) setOpen(false);
+        }
+
+        document.addEventListener("click", clickOutside);
+        return () => document.removeEventListener("click", clickOutside);
+    }, [open, setOpen]);
+
     return (
-        <aside id="sidebar" className="flex-1 border-r border-r-surface-300-700 drop-shadow-md">
+        <aside
+            id="sidebar"
+            ref={ref}
+            data-open={open}
+            className="fixed md:sticky z-10 transition-all min-h-screen max-h-screen max-w-60 duration-200 ease-in-out top-0 -left-50 data-[open=true]:left-0 md:flex-1 border-r border-r-surface-300-700 drop-shadow-md"
+        >
             <Navigation />
             {/* user data */}
         </aside>
-    )
+    );
+}
+
+export function ToggleSidebarButton() {
+    const { toggleOpen } = useSidebarContext();
+
+    return (
+        <button
+            className="block md:hidden"
+            id="open-sidebar"
+            type="button"
+            onClick={(e) => {
+                e.stopPropagation();
+                toggleOpen();
+            }}
+        >
+            <label className="sr-only" htmlFor="open-sidebar">
+                Open Sidebar
+            </label>
+            <MenuIcon />
+        </button>
+    );
 }
