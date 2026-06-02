ffc162a2b655d0770930ebbad53d753d1e61803b
ozzythecoder <august.chm@gmail.com>
2026-06-02 15:10:06 -0500

    feat: thread view with replies & formatting

--- apps/frontend/src/features/threads/components/ThreadView.tsx
+++ apps/frontend/src/features/threads/components/ThreadView.tsx
@@ -1,27 +1,60 @@
 import type { ForumMessage } from "#/types/resources";
+import { Link } from "@tanstack/react-router";
+import { DraftJsContent } from "../content/render";
+import "./thread.css";
+import { Avatar } from "#/components/ui/Avatar";
+import { ArrowLeft } from "lucide-react";
+import { utils } from "#/utils/utils";
+import { Gutter } from "#/components/ui/Gutter";
+import { ImageGrid } from "./ImageGrid";
 
 function ThreadEntry({ message }: { message: ForumMessage }) {
     return (
-        <div>
-            {message.author}
-        </div>
+        <li className="card relative p-4 sm:py-8 flex flex-col sm:flex-row gap-4 sm:gap-8 preset-filled-surface-100-900">
+            <div className="flex sm:flex-col items-center justify-between sm:min-w-16 md:min-w-24">
+                <div className="flex flex-row sm:flex-col items-center gap-2">
+                    <Avatar user={message.author ?? "Unknown User"} className="not-sm:size-8" />
+                    <p className="text-surface-600-400">{message.author}</p>
+                </div>
+                <time className="text-xs italic text-surface-600-400 sm:hidden">
+                    {utils.formatDate(message.created_at)}
+                </time>
+            </div>
+            <div>
+                <time className="text-xs italic text-surface-600-400 hidden sm:block">
+                    {utils.formatDate(message.created_at)}
+                </time>
+                <div className="_thread-content">
+                    <DraftJsContent rawContent={message.content} />
+                </div>
+                <ImageGrid images={message.images} />
+            </div>
+        </li>
     );
 }
 
 export function ThreadView({ thread, replies }: { thread: ForumMessage; replies: ForumMessage[] }) {
-    console.log("REPLIES:", replies)
+    console.log(thread.images);
     return (
-        <div>
-            <ul>
-                <li>
+        <Gutter>
+            <main className="mb-16">
+                <div className="flex flex-row items-center justify-between">
+                    <h2 className="h4 mb-4">{thread.title}</h2>
+                    <Link
+                        to="/threads"
+                        from="/threads/$id"
+                        className="text-sm anchor flex flex-row gap-2 items-center hover:text-primary-900-100"
+                    >
+                        <ArrowLeft className="inline size-4" /> <span>Back to Threads</span>
+                    </Link>
+                </div>
+                <ul className="flex flex-col gap-4">
                     <ThreadEntry message={thread} />
-                </li>
-                {replies.map((r) => (
-                    <li key={r.id}>
-                        <ThreadEntry message={r} />
-                    </li>
-                ))}
-            </ul>
-        </div>
+                    {replies.map((r) => (
+                        <ThreadEntry key={r.id} message={r} />
+                    ))}
+                </ul>
+            </main>
+        </Gutter>
     );
 }
