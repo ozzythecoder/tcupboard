4e0cd9f8e408ca9a5cca3b9669eb2c1658eda888
ozzythecoder <august.chm@gmail.com>
2026-06-02 19:18:28 -0500

    feat: thread list view now uses same compact avatar as individual thread view at smaller viewports

--- apps/frontend/src/features/threads/components/ThreadList.tsx
+++ apps/frontend/src/features/threads/components/ThreadList.tsx
@@ -1,5 +1,5 @@
 import type { ForumMessageWithReplyDetails } from "#/types/resources";
-import { Avatar } from "@skeletonlabs/skeleton-react";
+import { Avatar } from "#/components/ui/Avatar";
 import { utils } from "#/utils/utils";
 import { Link } from "@tanstack/react-router";
 
@@ -7,24 +7,19 @@ export function ThreadListItem({ thread }: { thread: ForumMessageWithReplyDetail
     const createdAt = utils.formatDate(thread.created_at);
 
     return (
-        <li className="card p-4 grid grid-cols-[auto_1fr] preset-filled-surface-100-900 has-[.-thread-title-anchor:hover]:preset-filled-primary-50-950 overflow-hidden">
-            <div className="flex flex-col items-center gap-4 w-25">
-                <Link to="/" from="/threads/">
-                    <Avatar className="size-14 hover:brightness-110">
-                        <Avatar.Image src={thread.author_avatar ?? undefined} />
-                        <Avatar.Fallback>{thread.author?.charAt(0).toUpperCase()}</Avatar.Fallback>
-                    </Avatar>
-                </Link>
-                <Link to="/" from="/threads/">
-                    <p className="anchor text-xs text-surface-400-600">{thread.author}</p>
-                </Link>
+        <li className="card p-4 flex flex-col gap-2 sm:grid sm:grid-cols-[auto_1fr] preset-filled-surface-100-900 has-[.-thread-title-anchor:hover]:preset-filled-primary-50-950 overflow-hidden">
+            <div className="flex sm:flex-col items-center justify-between sm:min-w-16 md:min-w-24">
+                <div className="flex flex-row sm:flex-col items-center gap-2">
+                    <Avatar user={thread.author ?? "Unknown User"} avatarUrl={thread.author_avatar} className="not-sm:size-8" />
+                    <p className="text-surface-600-400">{thread.author}</p>
+                </div>
             </div>
             <Link
                 to="/threads/$id"
                 params={{ id: thread.id!.toString() }}
                 className="-thread-title-anchor group"
             >
-                <div className="flex flex-col justify-between gap-4 h-full break-normal">
+                <div className="flex flex-col justify-between gap-2 md:gap-4 h-full break-normal">
                     <h3 className="h6 group-hover:underline">{thread.title}</h3>
                     <p className="text-xs text-surface-400-600">
                         {createdAt} &bull; {thread.replyCount ?? 0}{" "}
