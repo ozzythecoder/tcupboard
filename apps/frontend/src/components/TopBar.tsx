59a1f38a6c56a0271d36d7d6f4cb8ba413119f0a
ozzythecoder <august.chm@gmail.com>
2026-06-02 19:17:01 -0500

    links in top bar

--- apps/frontend/src/components/TopBar.tsx
+++ apps/frontend/src/components/TopBar.tsx
@@ -21,13 +21,17 @@ export function TopBar({ title, href }: Props) {
         <div className="grid grid-cols-3 md:flex items-center gap-2 px-2 sm:px-0 py-4">
             <SidebarToggle />
 
-            <h1 className="h6 xs:text-lg tracking-tight text-center block md:hidden">TCUPboard</h1>
+            <Link to="/">
+                <h1 className="h6 xs:text-lg tracking-tight text-center block md:hidden text-primary-800-200 hover:text-primary-800-200/80">
+                    TCUPboard
+                </h1>
+            </Link>
             {href ? (
-                <Link to={href} from="/">
-                    <h1 className="h1 text-2xl sm:text-3xl md:text-4xl tracking-tight text-right md:text-left hover:text-surface-800-200">
+                    <h1 className="text-right">
+                        <Link to={href} from="/" className="h1 text-2xl sm:text-3xl md:text-4xl tracking-tight md:text-left hover:text-surface-800-200">
                         {title ?? " "}
+                        </Link>
                     </h1>
-                </Link>
             ) : (
                 <h1 className="h1 text-2xl sm:text-3xl md:text-4xl tracking-tight text-right md:text-left">
                     {title ?? " "}
