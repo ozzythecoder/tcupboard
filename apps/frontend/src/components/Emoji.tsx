6d881072e09ee48e51777e6906e4a9cf9bf4baf1
ozzythecoder <august.chm@gmail.com>
2026-06-18 13:59:14 -0500

    feat: emoji component for rendering emoji proper

---
+++ apps/frontend/src/components/Emoji.tsx
@@ -0,0 +1,15 @@
+interface Props {
+    /**
+     * The hexadecimal UTF-8 code, without the leading 'U+', but WITH the initial 'x'.
+     */
+    code: string;
+    className?: string;
+}
+
+export function Emoji({ code, className  }: Props) {
+    if (code === 'love') code = 'x1f499';
+    const html = `&#${code};`
+    return (
+        <span className={`font-mono _emoji ${className}`} dangerouslySetInnerHTML={{ __html: html }} />
+    );
+}
