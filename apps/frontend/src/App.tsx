f180bc6772798d3b2676df707c13f3005afdcf5f
ozzythecoder <august.chm@gmail.com>
2026-06-18 13:59:00 -0500

    feat: use loading component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,12 +3,13 @@ import { useAuth0Context } from "./config/auth";
 import { queryClient } from "./config/api";
 import { router } from "./router";
 import { RouterProvider } from "@tanstack/react-router";
+import { Loading } from "./components/Loading";
 
 export function App() {
     const auth = useAuth0Context();
 
     if (auth.isLoading) {
-        return <div className="content-center min-h-screen">Loading...</div>;
+        return <Loading />;
     }
 
     return (
