29814cdaedd35c02a98b1f01441f6049fc780aae
ozzythecoder <august.chm@gmail.com>
2026-06-30 18:52:28 -0500

    delete old db artifacts

--- supabase/schemas/forum_message_replies.sql
+++
@@ -1,8 +0,0 @@
-create or replace function public.replies_by_thread(thread_id_in text)
-returns setof public.forum_messages
-as $$
-begin
-    return query select * from public.forum_messages where parent_id = thread_id_in::bigint;
-end;
-$$
-language plpgsql;
