e7c5ed9ad1c137006e71848262a7487c9d567160
ozzythecoder <august.chm@gmail.com>
2026-08-06 16:21:05 -0500

    (web) chore: colocated app route

--- apps/web/src/routes/_app/route.tsx
+++ apps/web/src/routes/_app/route.tsx
@@ -11,12 +11,10 @@ export const Route = createFileRoute("/_app")({
 
 function RouteComponent() {
     return (
-        <div>
-            <div className="flex flex-row">
-                <Sidebar />
-                <div id="content" tabIndex={-1} className="flex-5 md:flex-4 px-3 min-h-screen">
-                    <Outlet />
-                </div>
+        <div className="flex flex-row">
+            <Sidebar />
+            <div className="flex-5 md:flex-4 px-3 min-h-screen _page-gradient" id="content" tabIndex={-1}>
+                <Outlet />
             </div>
         </div>
     );
