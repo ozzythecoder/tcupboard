f987dbe134b7dcbcd1ee99e0de3666f159430a7f
ozzythecoder <august.chm@gmail.com>
2026-08-06 16:38:43 -0500

    (backend) feat: tcup updates implementation

---
+++ apps/backend/src/modules/tcup-updates/tcup-updates.route.ts
@@ -0,0 +1,38 @@
+import { isDirectusError } from "@directus/sdk";
+import express from "express";
+import { directus } from "@/config/cms.js";
+import { NotFoundError } from "@/types/errors.js";
+import { TcupUpdatesService } from "./tcup-updates.service.js";
+
+const tcupUpdatesRouter = express.Router();
+const svc = new TcupUpdatesService(directus);
+
+tcupUpdatesRouter.get("/", async (_req, res, next) => {
+    try {
+        const data = await svc.getAll();
+        return res.json(data);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+tcupUpdatesRouter.get("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const data = await svc.getById(id);
+        return res.json(data);
+    } catch (error) {
+        console.debug(error);
+        if (isDirectusError(error)) {
+            // Directus throws 'Forbidden' errors for not found resources
+            if (error.response.status === 403) {
+                console.warn("Rewriting Directus 403 to NotFoundError");
+                return next(new NotFoundError("News update not found"));
+            }
+        }
+        return next(error);
+    }
+});
+
+export { tcupUpdatesRouter };
