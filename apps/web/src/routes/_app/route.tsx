a6e0e55cf5615897896a83f0be170416075cbccf
ozzythecoder <august.chm@gmail.com>
2026-08-07 07:06:32 -0500

    (web) feat: add highlighted campaign to _app route

--- apps/web/src/routes/_app/route.tsx
+++ apps/web/src/routes/_app/route.tsx
@@ -1,8 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { Sidebar } from "#/components/Sidebar";
+import { campaignQueries } from "#/features/campaign/campaign.api";
+import { CampaignHighlightBanner, CampaignHighlightWrapper } from "#/features/campaign/components/CampaignHighlightBanner";
 
 export const Route = createFileRoute("/_app")({
     component: RouteComponent,
+    loader: () => {
+        const highlightOpts = campaignQueries.highlighted();
+        return {
+            highlightOpts,
+        };
+    },
 });
 
 /**
@@ -10,10 +19,18 @@ export const Route = createFileRoute("/_app")({
  */
 
 function RouteComponent() {
+    const { highlightOpts } = Route.useLoaderData();
+    const { data: highlightedCampaign } = useQuery(highlightOpts);
+
     return (
         <div className="flex flex-row">
+            {highlightedCampaign && <CampaignHighlightWrapper campaign={highlightedCampaign} />}
             <Sidebar />
-            <div className="flex-5 md:flex-4 px-3 min-h-screen _page-gradient" id="content" tabIndex={-1}>
+            <div
+                className="flex-5 md:flex-4 px-3 min-h-screen _page-gradient"
+                id="content"
+                tabIndex={-1}
+            >
                 <Outlet />
             </div>
         </div>
