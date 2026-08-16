c2172cabf3e68138a1ce3417dfa8079668a674a0
ozzythecoder <august.chm@gmail.com>
2026-08-16 14:51:09 -0500

    (backend) fix: site intro should be public

--- apps/backend/src/modules/globals/globals.route.ts
+++ apps/backend/src/modules/globals/globals.route.ts
@@ -1,17 +1,18 @@
 import express from "express";
 import { directus } from "@/config/cms.js";
+import { publicRoute } from "@/middleware/route.js";
 import { GlobalsService } from "./globals.service.js";
 
 const globalsRouter = express.Router();
-const svc = new GlobalsService(directus)
+const svc = new GlobalsService(directus);
 
-globalsRouter.get('/site-intro', async (_req, res, next) => {
-    try {
-        const intro = await svc.getSiteIntro();
-        res.json(intro);
-    } catch (error) {
-        next(error);
-    }
-})
+globalsRouter.get(
+    "/site-intro",
+    ...publicRoute({
+        handler: async (_req, res, next) => {
+            res.json(await svc.getSiteIntro().catch(next));
+        },
+    }),
+);
 
-export { globalsRouter }
+export { globalsRouter };
